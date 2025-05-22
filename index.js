733e84b639ddd7ba8a6edb816ef237d765aeec54
md arifur rahman <aimless.arif09@gmail.com>
2025-05-22 19:50:44 +0600

    push code before deploy on vercel

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function run() {
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
         
     } finally {
         // Ensures that the client will close when you finish/error
@@ -90,6 +90,6 @@ app.get("/", (req, res) => {
     res.send("hello flora nest")
 })
 
-app.listen(port, () => {
-    console.log(`My port is ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`My port is ${port}`)
+// })
